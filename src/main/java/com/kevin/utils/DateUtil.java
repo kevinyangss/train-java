da4d5f759954e78958f9ae5c83d23630771623ad
yangshuaishuai <ss.yang@datsch.com.cn>
2020-12-11 20:49:37 +0800

    util

---
+++ src/main/java/com/kevin/utils/DateUtil.java
@@ -0,0 +1,10 @@
+package com.kevin.utils;
+
+/**
+ * @ClassName DateUtil
+ * @Description 时间工具类
+ * @Author kevin
+ * @Date 2020-12-11 20:48
+ */
+public class DateUtil {
+}
---
+++ src/main/java/com/kevin/utils/MathUtil.java
@@ -0,0 +1,10 @@
+package com.kevin.utils;
+
+/**
+ * @ClassName MathUtil
+ * @Description 计算工具类
+ * @Author kevin
+ * @Date 2020-12-11 20:49
+ */
+public class MathUtil {
+}
