1b7caaf2b587a238f56e471087eb4088eb65748a
yangshuaishuai <ss.yang@datsch.com.cn>
2021-01-25 11:57:56 +0800

    dateutil

--- src/main/java/com/kevin/utils/DateUtil.java
+++ src/main/java/com/kevin/utils/DateUtil.java
@@ -3,6 +3,7 @@ package com.kevin.utils;
 import com.kevin.base.BaseApp;
 
 import java.sql.Timestamp;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -24,7 +25,7 @@ public class DateUtil extends BaseApp{
     public final static String TIMESTAMP_PATTERN = "HH:mm:ss";
     public final static String YYYYMMDD = "YYYYMMdd";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Date date = new Date();
 
         println("nowTime", true);
@@ -40,12 +41,22 @@ public class DateUtil extends BaseApp{
         System.out.println(nowTimestamp);
 
         println("formatyyMMdd", true);
-        formatyyMMdd(yyMMdd);
+        String formatyyMMdd = formatyyMMdd(yyMMdd);
+        System.out.println(formatyyMMdd);
+
+        println("getStartTimeOfDay", true);
+        Date getStartTimeOfDay = dateFormatDate(date);
+        System.out.println(getStartTimeOfDay);
+        System.out.println(format(getStartTimeOfDay, DATETIME_PATTERN));
 
         println("getEndTimeOfDay", true);
         Date endTimeOfDay = getEndTimeOfDay(date);
         System.out.println(endTimeOfDay.getTime());
         System.out.println(format(endTimeOfDay, DATETIME_PATTERN));
+
+        println("addDay", true);
+        Date addDay = addDay(date, 1);
+        System.out.println(format(addDay, DATETIME_PATTERN));
     }
 
     /**
@@ -85,6 +96,17 @@ public class DateUtil extends BaseApp{
     }
 
     /**
+     * 日期转换：如2016-12-12 15:34:34 改成 2016-12-12 00:00:00
+     * @param date
+     * @return
+     * @throws ParseException
+     */
+    public static Date dateFormatDate(Date date) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat(yyyyMMdd);
+        return sdf.parse(sdf.format(date));
+    }
+
+    /**
      * 获取date所在日的23:59:59
      * @param date
      * @return
@@ -109,5 +131,31 @@ public class DateUtil extends BaseApp{
         cal.setTimeInMillis(cal.getTimeInMillis() - millisecond);
         // 凌晨23:59:59
         cal.setTimeInMillis(cal.getTimeInMillis() + 23 * 60 * 60 * 1000 + 59 * 60 * 1000 + 59 * 1000);
+    }
+
+    /**
+     * 天数加减
+     * @param date
+     * @param days
+     * @return
+     * @throws ParseException
+     */
+    public static Date addDay(Date date, int days)
+            throws ParseException {
+        return addTime(date, Calendar.DATE, days);
+    }
+
+    /**
+     * 日期加减
+     * @param date 需要加减的日期
+     * @param type 类型（年--Calendar.YEAR、月--Calendar.MONTH、日--Calendar.DATE）
+     * @param num 加减数量（负数为减）
+     * @return
+     */
+    public static Date addTime(Date date, int type, int num) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(type, num);
+        return cal.getTime();
     }
 }
