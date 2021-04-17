0b84f4e9c30a76df3c4ba678c6609581fb06499f
yangshuaishuai <ss.yang@datsch.com.cn>
2021-04-17 17:42:57 +0800

    org.apache.commons.collections4

---
+++ src/main/java/com/kevin/apache/collections/BagTest.java
@@ -0,0 +1,37 @@
+package com.kevin.apache.collections;
+
+import com.kevin.base.BaseApp;
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.bag.HashBag;
+
+/**
+ * Bag - Bag界面简化了每个对象具有多个副本的集合。
+ * Bag定义了一个集合，用于计算对象在集合中出现的次数。
+ *
+ * @ClassName BagTest
+ * @Description TODO
+ * @Author kevin.yang
+ * @Date 2021-04-17 17:32
+ */
+public class BagTest extends BaseApp {
+
+    public static void main(String[] args) {
+        Bag<String> bag = new HashBag<>();
+        bag.add("a" , 2);
+        bag.add("b");
+        bag.add("c");
+        bag.add("d",3);
+
+        System.out.println("d is present " + bag.getCount("d") + " times.");
+        System.out.println("bag: " +bag);
+        System.out.println("Unique Set: " +bag.uniqueSet());
+
+        println("", true);
+
+        bag.remove("d",2);
+        System.out.println("2 occurences of d removed from bag: " +bag);
+        System.out.println("d is present " + bag.getCount("d") + " times.");
+        System.out.println("bag: " +bag);
+        System.out.println("Unique Set: " +bag.uniqueSet());
+    }
+}
